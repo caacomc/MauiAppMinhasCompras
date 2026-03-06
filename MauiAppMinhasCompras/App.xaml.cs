0a862f1798ea5f4c023212bd43b285745cd04885
caroline.campos <caacomc@gmail.com>
2026-03-06 15:40:49 -0300

    alteração pagia novo produto

--- MauiAppMinhasCompras/App.xaml.cs
+++ MauiAppMinhasCompras/App.xaml.cs
@@ -1,7 +1,26 @@
-﻿namespace MauiAppMinhasCompras
+﻿using MauiAppMinhasCompras.Helpers;
+
+namespace MauiAppMinhasCompras
 {
     public partial class App : Application
     {
+        static SQliteDatabaseHelper _db;
+
+        public static SQliteDatabaseHelper Db
+        {
+            get
+            {
+                if (_db == null)
+                {
+                    string path = Path.Combine(
+                        Environment.GetFolderPath(Environment.SpecialFolder.LocalApplicationData),
+                        "banco_sqlite_compras.db3");
+
+                    _db = new SQliteDatabaseHelper(".....db3");
+                }
+                return _db;
+            }
+        }
         public App()
         {
             InitializeComponent();
--- MauiAppMinhasCompras/Views/NovoProduto.xaml.cs
+++ MauiAppMinhasCompras/Views/NovoProduto.xaml.cs
@@ -1,9 +1,35 @@
+using MauiAppMinhasCompras.Models;
+
 namespace MauiAppMinhasCompras.Views;
 
 public partial class NovoProduto : ContentPage
 {
-	public NovoProduto()
-	{
-		InitializeComponent();
-	}
+    public NovoProduto()
+    {
+        InitializeComponent();
+    }
+
+    // Método executado quando o botão do Toolbar é clicado
+    private async void ToolbarItem_Clicked(object sender, EventArgs e)
+    {
+        try
+        {
+            // Cria um novo objeto Produto com os dados digitados nos campos
+            Produto p = new Produto
+            {
+                Descricao = txt_descricao.Text,
+                Quantidade = Convert.ToDouble(txt_quantidade.Text), // Converte quantidade para número
+                Preco = Convert.ToDouble(txt_preco.Text) // Converte preço para número
+            };
+            // Insere o produto no banco de dados
+            await App.Db.Insert(p);
+            // Exibe mensagem de sucesso ao salvar
+            await DisplayAlert("Sucesso!", "Registro Inserido", "OK");
+
+        } catch (Exception ex)
+        {
+            // Caso ocorra erro, mostra a mensagem na tela
+            await DisplayAlert("Ops", ex.Message, "OK");
+        }
+    }
 }
